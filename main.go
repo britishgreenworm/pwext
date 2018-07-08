62f6b4f307a5b494dc69dd576c10e9feed64a51d
britishgreenworm <britishgreenworm@gmail.com>
2018-07-08 15:02:00 -0400

    added everything

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	args := os.Args[1:]
+
+	if args[0] == "/help" || args[0] == "--help" || args[0] == "-h" || args[0] == "-help" {
+		fmt.Println("\nExtracts passwords from a file.")
+		fmt.Println()
+		fmt.Println("usage: pwext {file location} {password length} {exluded characters} {optional: ignore prefix length}")
+		return
+	}
+
+	if len(args) < 3 {
+		fmt.Println("Arguments are required, use format: pwext {file location} {password length} {exluded characters} {optional: ignore prefix length}")
+		return
+	}
+
+	fileLocation := args[0]
+	passwordLength, _ := strconv.Atoi(args[1])
+	excludeRunes := args[2]
+	ignorePrefix := 0
+
+	if len(args) > 3 {
+		ignorePrefix, _ = strconv.Atoi(args[3])
+	}
+
+	file, err := os.Open(fileLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for line := 1; scanner.Scan(); line++ {
+		runes := []rune(scanner.Text())
+
+		for x := ignorePrefix; x <= (len(scanner.Text()) - 14); {
+			if !strings.ContainsAny(string(runes[x:(x+passwordLength)]), excludeRunes) {
+				fmt.Println(string(runes[x : x+passwordLength]))
+				x += 14
+			} else {
+				x++
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
